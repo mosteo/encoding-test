8a831de156e3546d3b036be4d8ce809f0f308080
Alejandro R. Mosteo <amosteo@unizar.es>
2019-10-02 17:53:33 +0200

    Output hexa

--- src/list.adb
+++ src/list.adb
@@ -1,3 +1,4 @@
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with Ada.Text_IO; use Ada.Text_IO;
 
 with GNATCOLL.VFS; use GNATCOLL.VFS;
@@ -7,9 +8,15 @@ procedure List is
              Read_Dir_Recursive (Get_Current_Dir);
 begin
    for File of Files.all loop
-      Put_Line (String (Base_Name (File)) & " "
-                & String (Base_Name (File))'Length'Img & " "
-                & Display_Base_Name (File) & " "
-                & Display_Base_Name (File)'Length'Img);
+      Put (String (Base_Name (File)) & " "
+           & String (Base_Name (File))'Length'Img & " "
+           & Display_Base_Name (File) & " "
+           & Display_Base_Name (File)'Length'Img);
+      Put (" ");
+      for Char of Display_Base_Name (File) loop
+         Put (Character'Pos (Char), Width => 2, Base => 16);
+         Put (" ");
+      end loop;
+      New_Line;
    end loop;
 end List;
